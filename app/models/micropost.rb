eb7d50128da252505c32b27972094200c73d714c
Grant Adamson <grant_adamson@me.com>
2010-12-11 23:29:34 -0800

    Finished the demo app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ActiveRecord::Base
+  belongs_to :user
+  MIN , MAX = 3, 140
+  validates_length_of :content, :within => MIN..MAX, :on => :create,
+    :message => "must be between #{MIN} and #{MAX} characters"
+  validates_presence_of :user_id, :on => :create, :message => "can't be blank"
 end
